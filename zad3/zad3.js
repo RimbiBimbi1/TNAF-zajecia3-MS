7dad7b4007223f448cc4d80449c7163dbffbd015
RimbiBimbi1 <103674299+RimbiBimbi1@users.noreply.github.com>
2022-11-29 10:51:15 +0100

    2022112905

--- zad3/zad3.js
+++ zad3/zad3.js
@@ -2,10 +2,10 @@
 
 const range = (a,b) => {
     let array = [];
-    for(i=a;i<=b;i++){
+    for(i=Math.ceil(a);i<=b;i++){
         array.push(i);
     }
     return array;
 }
 
-console.log(range(2,5))
+console.log(range(3.000001,5))
