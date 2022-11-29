5a7c23715b2a21fa21ed1fd43464182b4a142778
RimbiBimbi1 <103674299+RimbiBimbi1@users.noreply.github.com>
2022-11-29 09:21:16 +0100

    2022112901

---
+++ zad1/functions.js
@@ -0,0 +1,15 @@
+function isOdd(a){
+    return (a%2) === 1;
+}
+
+const isEven = a => (a%2) === 0;
+
+
+console.log("isOdd(3)")
+console.log(isOdd(3));
+console.log("isOdd(4)")
+console.log(isOdd(4));
+console.log("isEven(5)")
+console.log(isEven(5));
+console.log("isEven(6)")
+console.log(isEven(6));
